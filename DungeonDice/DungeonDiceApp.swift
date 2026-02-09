83ea62f72f7d4653d4d58d434baf6d6d3b96ffa1
gabrielabizarro <gbizarro2027@gmail.com>
2026-02-09 12:54:15 -0800

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by BIZARRO, GABRIELA on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
