c98827194690198e8fa35e7e78cc3f2a8ab7b3e8
gabrielabizarro <gbizarro2027@gmail.com>
2026-02-13 12:42:06 -0800

    02/13/26

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -8,12 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    enum Dice: Int, CaseIterable {
+        case four = 4
+        case six = 6
+        case eight = 8
+        case ten = 10
+        case twelve = 12
+        case twenty = 20
+        case hundred = 100
+
+        func roll() -> Int {
+            return Int.random(in: 1...self.rawValue)
+        }
+    }
+
+    @State private var resultMessage = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dungeon Dice")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.red)
+            
+            Spacer()
+            
+            Text(resultMessage)
+                .font(.largeTitle)
+                .fontWeight(.medium)
+                .multilineTextAlignment(.center)
+                .frame(height: 150)
+            
+            Spacer()
+            
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))]) {
+                ForEach(Dice.allCases, id: \.self) { die in  Button("\(die.rawValue)-sided"){
+                    resultMessage = "You rolled a \(die.roll()) on a \(die.rawValue)-sided dice"
+                }
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+            }
+            
+            
         }
         .padding()
     }
